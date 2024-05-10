66bfb0dfbe244ae23f5717b980ae795996f38aa8
LE3U <157618243+LE3U@users.noreply.github.com>
2024-05-10 12:23:58 +0800

    Update2 index.tsx

--- src/components/patient-information-list/index.tsx
+++ src/components/patient-information-list/index.tsx
@@ -1,36 +1,34 @@
-import { PatientModel } from '@/models/PatientModel'
 import { Box, Card, Flex, Text } from '@radix-ui/themes'
 import PatientInformation from '../patient-information'
 import { db } from '@/firebase'
 import { FirebaseDocument } from '@/models/FirebaseDocuments'
-import { collection, onSnapshot, query, orderBy, Timestamp } from 'firebase/firestore'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 
 const PatientInformationList = () => {
-
     const [patients, setPatients] = useState([] as Array<FirebaseDocument>)
 
     useEffect(() => {
-        const q = query(collection(db, 'patients'), orderBy('createdAt', 'asc')); // Assuming createdAt is the timestamp field
+        const q = query(collection(db, 'patients'), orderBy('createdAt', 'desc')); // Ordering by 'createdAt' in descending order
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             let arr: Array<FirebaseDocument> = []
 
             querySnapshot.forEach(doc => {
-                arr.push(
-                    {
-                        data: doc.data(),
-                        id: doc.id
-                    } as FirebaseDocument
-                )
+                arr.push({
+                    data: doc.data(),
+                    id: doc.id
+                } as FirebaseDocument)
             });
             setPatients(arr)
         })
+
+        return () => unsubscribe(); // Cleanup function to unsubscribe from snapshot listener
     }, [])
 
     return (
         <Card>
-            <Flex direction={'column-reverse'} justify={'center'} gap={'2'}>
-                <Box display={`${patients.length > 0 ? 'none' : 'block'}`}>
+            <Flex direction={'column'} justify={'center'} gap={'2'}>
+                <Box display={`${patients.length === 0 ? 'block' : 'none'}`}>
                     <Text weight={'bold'}>Patient List is empty. </Text>
                 </Box>
                 {patients.map((value: FirebaseDocument) => (
